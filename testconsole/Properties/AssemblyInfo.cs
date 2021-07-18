8a874fac82ed787d2cc3b96a268bb16afede5da7
Uladzimir Zakharenkau <Uladzimir_Zakharenkau@epam.com>
2021-07-18 12:38:33 +0300

    Adding SubsetHelper functionallity and some tests

--- testconsole/Properties/AssemblyInfo.cs
+++ testconsole/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.2.2.4")]
-[assembly: AssemblyFileVersion("3.2.2.4")]
+[assembly: AssemblyVersion("3.2.2.9")]
+[assembly: AssemblyFileVersion("3.2.2.8")]
